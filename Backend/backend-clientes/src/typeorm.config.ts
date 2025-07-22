4f9ffc47ea554977f2deb2198407c655aa417149
Kevin Olivella <kevinolivella@gmail.com>
2025-07-22 13:27:42 -0500

    Se actualiza el typeorm

--- Backend/backend-clientes/src/typeorm.config.ts
+++ Backend/backend-clientes/src/typeorm.config.ts
@@ -4,11 +4,11 @@ import { Cliente } from './clientes/cliente.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'KevDeb93',
-  database: 'clientesdb',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [Cliente],
   synchronize: true
 };
